46683c159093fe2df4bd107c952b99586c9b014c
KarenLopezB <53828090+KarenLopezB@users.noreply.github.com>
2021-09-02 17:31:07 -0700

    Add files via upload

---
+++ Recursos/Recursos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Recursos
+//
+//  Created by Alumno on 9/2/21.
+//  Copyright © 2021 ulsa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
